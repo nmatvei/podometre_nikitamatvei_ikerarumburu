f2a54125086abb5c2cc4f437724cde1f97044637
ikkxeer <samuiker2427@gmail.com>
2025-11-27 15:16:53 +0100

    Boton reiniciar

--- app/src/main/java/com/example/podometre_nikitamatvei_ikerarumburu/MainActivity.java
+++ app/src/main/java/com/example/podometre_nikitamatvei_ikerarumburu/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.podometre_nikitamatvei_ikerarumburu;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class MainActivity extends AppCompatActivity {
+
+    int metresRecorreguts = 0;
+    int numPassos = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView textPassos = (TextView) findViewById(R.id.textView2);
+        TextView textMetres = (TextView)findViewById(R.id.textView3);
+        Button btnReiniciar = (Button)findViewById(R.id.button);
+
+        btnReiniciar.setOnClickListener(v -> {
+            numPassos = 0;
+            metresRecorreguts = 0;
+
+            textPassos.setText("Passos comptabilitzats: " + numPassos);
+            textMetres.setText("Metres recorreguts: " + metresRecorreguts);
+        });
+
+
     }
 }
